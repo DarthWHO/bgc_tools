8fceec01a54626655f9ba3d8733a83894d43994c
Scott Tibbo <40707785+DarthWHO@users.noreply.github.com>
2026-02-01 11:35:21 -0700

    fixed margin issues

--- src/pages/Middara.jsx
+++ src/pages/Middara.jsx
@@ -2,9 +2,9 @@ import { Typography, Box } from "@mui/material";
 
 const Middara = () => {
   return (
-    <Box sx={{ flexGrow: 1, p: 1 }}>
-      <Typography variant="h4" gutterBottom>
-        Middara (Coming Soon)
+    <Box sx={{ p: 4, maxWidth: "100%", margin: "0 auto" }}>
+      <Typography variant="h4">
+        Middara Character Tracker (Coming Soon)
       </Typography>
     </Box>
   );
